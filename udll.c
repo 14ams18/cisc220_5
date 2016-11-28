85a710efb272ff6bf671571f0c8b8d01a4fb5fe1
14ams18 <abigael.schonewille@hotmail.com>
2016-11-28 18:24:52 -0500

    Need to fill in the functions

---
+++ udll.c
@@ -0,0 +1,25 @@
+union Data {
+        // look at notes
+};
+
+struct Node {
+        //type for address?
+        //where instantiate
+};
+
+void insert(int index, union Data data){
+        //include length incrementer
+}
+
+void remove(int index){
+        //decrement length
+}
+
+union Data get(int index){
+        //minimum num of iterations
+        //if to start at head or tail: if index < length-1-index
+}
+
+int length(){
+        // global var declared in here?
+}
