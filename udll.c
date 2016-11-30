c479caf9d6eaf4d39ee209c1d50ef297ba81ed71
xXLinuxGodXx <connorway97@gmail.com>
2016-11-30 00:59:50 -0500

    Update udll.c

--- udll.c
+++ udll.c
@@ -13,10 +13,14 @@ void insert(int index, union Data data){
                 for(int i=0; i< (index-1); i++) {
                         ptr=ptr->next;
                 }
-                
+                Data *after = ptr->next->next;
+                Data newNode = {ptr, data, after}; //also a NULL for length
+                ptr.next = newNode;
+                after.previous = newNode;
+                head.length++;
         }
         else {
-                
+                printf("Invalid index value");
         }
                 
         //include length incrementer
