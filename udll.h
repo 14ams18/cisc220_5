6b64d73fda5737c18c0893641c4c8bab994f2c9e
14ams18 <abigael.schonewille@hotmail.com>
2016-12-01 23:28:43 -0500

    Update udll.h

--- udll.h
+++ udll.h
@@ -1,12 +1,16 @@
 #ifndef UDLL_H
 #define UDLL_H
 
-        void insert(int index, union Data data);
+        union Data;
 
-        void remove(int index);
+        struct Node;
 
-        union Data get(int index);
+        void insert(int index, union Data data, struct Node *head, struct Node *last);
 
-        int length();
+        void Remove(int index, struct Node *head, struct Node *last);
+
+        union Data get(int index, struct Node *head, struct Node *last);
+
+        int length (struct Node *head);
 
 #endif
