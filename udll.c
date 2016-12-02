7ece1df2b3dd329e27bf2d8a15fb46b94cfe4978
14ams18 <abigael.schonewille@hotmail.com>
2016-12-01 23:28:06 -0500

    Update udll.c

--- udll.c
+++ udll.c
@@ -1,103 +1,119 @@
-union Data {
-        // look at notes
+#include<stdlib.h>
+#include<stdio.h>
+
+union Data{
+        int data;
+        int *data;
+        float data;
+        float *data;
+        char data;
+        char *data;
 };
 
-typedef struct{
+struct Node{
         int length;
-        *Node previous;
-        Data data;
-        *Node next;
-        //type for address?
-        //where instantiate? as a global var in here?
-} Node;
+        struct Node *previous;
+        union Data data;
+        struct Node *next;
+};
 
-void insert(int index, union Data data){
-        if(*head.data == NULL){ //no linked list, creates node/reassigns 'head' and 'last'
-                Node newNode = {1, NULL, data, NULL};
-                *head = &newNode;
-                *last = &newNode;
+void insert(int index, union Data data, struct Node *head, struct Node *last){
+        if(!head){ //no linked list, creates node/reassigns 'head' and 'last'
+                struct Node newNode = {1, NULL, data, NULL};
+                head = &newNode;
+                last = &newNode;
+                head->length++;
         }
         else if(index == 0) { //inserting at front of the list
-                Node *ptr = *head;
-                Node newNode = {*head.length, NULL, data, *ptr};
-                ptr.previous = &newNode;
+                int len = head->length;
+                struct Node *ptr = head;
+                struct Node newNode = {head->length, NULL, data, ptr};
+                ptr->previous = &newNode;
                 head = &newNode;
+                head->length = len + 1;
         }
-        else if(index == head.length) { //inserting at end of the list
-                Node newNode = {NULL, *last, data, NULL}
-                last.next = &newNode;
+        else if(index == head->length) { //inserting at end of the list
+                struct Node newNode = {0, last, data, NULL};
+                last->next = &newNode;
                 last = &newNode;
+                head->length++;
         }
-        else if(index <= head.length) { //inserting in middle of list
-                Node *ptr = head;
-                for(int i=0; i< (index-1); i++) {
-                        ptr=ptr->next;
+        else if(index <= head->length) { //inserting in middle of list, lest loops, need to access from tail too
+                struct Node *ptr;
+                if(index <= head->length/2){
+                        ptr = head;
                 }
-                Node *after = ptr->next->next;
-                Node newNode = {NULL, ptr, data, after};
-                *ptr.next = newNode;
-                *after.previous = newNode;
+                else {
+                        ptr = last;
+                }
+                for(int i=0; i< (index-1); i++) {
+                                ptr=ptr->next;
+                }
+                struct Node *after = ptr->next->next;
+                struct Node newNode = {0, ptr, data, after};
+                ptr->next = &newNode;
+                after->previous = &newNode;
         }
         else { //no valid index provided
                 printf("Invalid index value");
         }
-        head.length++;
-                
-        //include length incrementer
-        //create list if none
+        head->length++;
 }
 
-void remove(int index){
+void Remove(int index, struct Node *head, struct Node *last){
         if(index == 0) { //removing head of list
-                head.next.length = head.length;
-                *head = *head.next;
-                *head.previous = NULL;
+                head->next->length = head->length;
+                head = head->next;
+                head->previous = NULL;
         }
-        else if(index == head.length) { //removing the last Node
-                *last = *last.previous;
-                last.next = NULL;
+        else if(index == head->length) { //removing the last Node
+                last = last->previous;
+                last->next = NULL;
         }
-        else if(index <= head.length) { //removing a Node in the middle
-                Node *ptr = head;
+        else if(index <= head->length) { //removing a Node in the middle
+                struct Node *ptr;
+                if(index <= head->length/2){
+                        ptr = head;
+                }
+                else {
+                        ptr = last;
+                }
                 for(int i=0; i< (index-1); i++) {
                         ptr=ptr->next;
                 }
-                Node *after = *ptr->next->next;
-                ptr.next = *after;
-                after.previous = *ptr;
+                struct Node *after = ptr->next->next;
+                ptr->next = after;
+                after->previous = ptr;
         }
         else { //no valid index provided
                 printf("Invalid index value");
         }
-        head.length--;
-        //decrement length
+        head->length--;
 }
 
-union Data get(int index){
-        if (index <= length/2) {
-                for(int i=0; i< index; i++) {
+union Data get(int index, struct Node *head, struct Node *last){
+        struct Node *ptr;
+        if (index <= head->length/2) {
+        for(int i=0; i< index; i++) {
                         ptr=ptr->next;
                 }
-                return ptr.data;
+                return ptr->data;
         }
-        else if (index > length/2) {
+        else if (index > head->length/2) {
                 for(int i=0; i< index; i++) {
                         ptr=ptr->previous;
                 }
-                return ptr.data;
+                return ptr->data;
         }
         else { //no valid index provided
                 printf("Invalid index value");
-                return NULL;
+                exit(-1);
         }
-        //minimum num of iterations
-        //if to start at head or tail: if index < length-1-index
 }
 
-int length(){
-        if(*head.length == NULL) {
-                return 0;
+int length (struct Node *head){
+        if(head->length) {
+                return head->length;
         }
-        return head.length;
-        // global var declared in here?
-}
+        return 0;
+
