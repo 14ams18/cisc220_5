1a756473a76df1cdc721232c4e72394c6ee8921e
14ams18 <abigael.schonewille@hotmail.com>
2016-12-01 23:25:52 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,19 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "udll.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include"udll.h"
 
 int main() {
+        union Data first=1;
+        struct Node *head = NULL;
+        struct Node *last = NULL;
+        insert(0, first, head, last);
+        get(0, head, last);
+        int len;
+        len = length(head);
+        printf("%d", len);
+        Remove(0, head, last);
+        get(0, head, last);
         return 0;
 
 }
+
